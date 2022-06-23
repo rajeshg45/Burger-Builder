4b5601161a209d2a67c79c7274948f004400b87f
Neshan <neshanparvej@gmail.com>
2022-06-23 17:41:50 +0600

    showing price on card

--- src/Components/BurgerBuilder/Burger/Burger.js
+++ src/Components/BurgerBuilder/Burger/Burger.js
@@ -1,13 +1,24 @@
 import React from "react";
+import './Burger.css';
 import Ingredient from "../Ingredient/Ingredient";
 
 const Burger = props => {
+    let ingredientArr = props.ingredients.map(item => {
+        let amountArr = [...Array(item.amount).keys()];
+        return amountArr.map(_ => {
+            return <Ingredient type={item.type} key={Math.random()} />
+        })
+    })
+        .reduce((arr, element) => {
+            return arr.concat(element);
+        }, []);
+    if (ingredientArr.length === 0) {
+        ingredientArr = <p>Please add some ingredient</p>
+    }
     return (
-        <div>
+        <div className="Burger">
             <Ingredient type="bread-top" />
-            <Ingredient type="meat" />
-            <Ingredient type="salad" />
-            <Ingredient type="cheese" />
+            {ingredientArr}
             <Ingredient type="bread-bottom" />
         </div>
     )
